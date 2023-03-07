d1952da5fad77293ca56290e13e8181aa24a7fb4
lzhphantom <987090402@qq.com>
2023-03-07 21:32:31 +0800

    2023年3月7日21:32:20

--- lzhphantom-user/user/src/main/java/com/lzhphantom/user/login/controller/UserController.java
+++ lzhphantom-user/user/src/main/java/com/lzhphantom/user/login/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.lzhphantom.user.login.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
 public class UserController {
+    @GetMapping("/test")
+    public String test(){
+        return "test ok";
+    }
 }
